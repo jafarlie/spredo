f7efad5b4dae2666caf853a26f4f1e8db19bdcf1
jafarlie <jafarli.elvin1997@gmail.com>
2023-09-14 22:57:17 -0400

    Implemented barebones of navigation menu

---
+++ spredo/src/components/navigation/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Navbar from "./navbar";
+import { useState } from "react";
+
+const Navigation = () => {
+  return(
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Navigation;
--- spredo/tailwind.config.ts
+++ spredo/tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      nutty: '#dba76c',
+      darkJelly: '#640223',
+      midnightGreen: '#05393c',
+      bread: '#fff7e1',
+      light: '#ffffff'
+    },
   },
   plugins: [],
 }
